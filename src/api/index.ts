420d24d96f775cd1e4e1c957a6ddee68f7616c23
HadHod <maciej.mlynarski@gmail.com>
2020-05-31 13:28:12 +0200

    sample api docs added

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,11 +3,27 @@ import films from '../db/films';
 
 const router: Router = express.Router();
 
-router.get('/movies', (_req: Request, res: Response) => {
+/**
+ * @api {get} /api/films Request Films list
+ * @apiName GetFilms
+ * @apiGroup Films
+ *
+ * @apiSuccess {Film[]} list of objects of the Film.
+ */
+router.get('/films', (_req: Request, res: Response) => {
     res.json(films);
 });
 
-router.get('/movies/:id', (req: Request, res: Response) => {
+/**
+ * @api {get} /api/films/:id Request User information
+ * @apiName GetFilms
+ * @apiGroup Films
+ *
+ * @apiParam {Number} id Films unique ID.
+ *
+ * @apiSuccess {Film} objects of the Film.
+ */
+router.get('/films/:id', (req: Request, res: Response) => {
     const film = films.find(f => f.id === req.params.id);
     if (film) {
         res.json(film);
