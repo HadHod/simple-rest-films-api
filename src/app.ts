58e59d1f74d96f0b53cc1d8dd32de0c83b2fd13f
HadHod <maciej.mlynarski@gmail.com>
2020-05-31 11:19:17 +0200

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 import routes from './routes';
 import api from './api';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 app.use('/api', api);
